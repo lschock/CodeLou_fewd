8a4eb0c5c6d16da20c0bf866110c101645916bb9
Laurie <lrschock2@gmail.com>
2018-03-08 21:09:26 -0500

    fixed mobile responsiveness

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
 
-$(document).ready (function () {
-
-//Lightbox//
-$(document).on('click', '[data-toggle="lightbox"]', function(event) {
-    event.preventDefault();
-    $(this).ekkoLightbox();
-});
-
-// Adding dialog box for About, Contacts, and Newsletter sections//
-$(".dialog").click(function(event){
-    $("#dialogbox").dialog({
-        width: 300,
-        height: 200,
-        draggable: false,
-        modal: true,  //makes it so the page dims and you cannot click on it - no interaction other than box//
-        show: {
-            effect: 'fade',
-            duration: 1000
-        },
-        hide: {
-            effect: 'blind',
-            duration: 500
-        },
-        buttons: {
-            Ok: function() {  //Ok will be written on the button//
-                $(this).dialog("close");
-            },
-        }
+$(document).ready(function () {
+    //Reposition at top of page on refresh//
+    $(window).on('beforeunload', function () {
+        $(window).scrollTop(0);
     });
-    }); 
+
+    //Lightbox Photo Gallery//
+    $(document).on('click', '[data-toggle="lightbox"]', function (event) {
+        event.preventDefault();
+        $(this).ekkoLightbox();
+    });
+
+
+    // Adding dialog box for About, Contacts, and Newsletter sections//
+    $(".dialog").click(function (event) {
+        $("#dialogbox").dialog({
+            width: 300,
+            height: 200,
+            draggable: false,
+            modal: true,  //makes it so the page dims and you cannot click on it - no interaction other than box//
+            show: {
+                effect: 'fade',
+                duration: 1000
+            },
+            hide: {
+                effect: 'blind',
+                duration: 500
+            },
+            buttons: {
+                Ok: function () {  //Ok will be written on the button//
+                    $(this).dialog("close");
+                },
+            }
+        });
+    });
 });// end ready
